48c7696b7206bde6567fa64d1043e227d7ea1c2a
DanilEzhov <50297244+florentiner@users.noreply.github.com>
2022-08-21 00:07:12 +0300

    Update main.cpp
    add possibility to specify directory whith results

--- analyzer/main.cpp
+++ analyzer/main.cpp
@@ -25,15 +25,22 @@ int main(int argc, char *argv[]) {
             return -1;
         }
 
+        std::string path_of_resaults = argv[argc-1]
         std::size_t num_threads = 1;
-        for (int i = 0; i < argc; i++)
+        for (int i = 0; i < argc; i++){
+            //Set number of threads (default 1)
             if (std::string(argv[i]).find("-t") != std::string::npos){
                 std::string raw_n_of_threads = std::string(argv[i]).substr(3 , std::string(argv[i]).size());
                 if(raw_n_of_threads == "all") raw_n_of_threads = std::to_string(std::thread::hardware_concurrency());
                 num_threads = std::stoi(raw_n_of_threads);
             }
+            //Set directory where will be results
+            if (std::string(argv[i]).find("-v") != std::string::npos){
+               path_of_resaults = std::string(argv[i]).substr(3 , std::string(argv[i]).size());
+            }
+        }
         omp_set_num_threads(num_threads);
-        return hydro_life(argv[argc-1], is_gz);
+        return hydro_life(path_of_resaults, is_gz);
     }
     else{
         std::cout << "Pleas enter input file (text format or .gz. Text format is about 3x faster then .gz)" << std::endl;
